79bd88ae11a5e45c0e9935239073642a724a197a
Ian Pearce <ian@ianpearce.org>
2022-09-19 09:30:02 -0700

    Use upper right for = and -

--- keyboards/boardsource/lulu/keymaps/tonytype/keymap.c
+++ keyboards/boardsource/lulu/keymaps/tonytype/keymap.c
@@ -17,9 +17,9 @@ enum custom_keycodes {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   =  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | ENTR |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   -  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | LCTL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------| MO(3) |    | TG(2) |------+------+------+------+------+------|
@@ -31,8 +31,8 @@ enum custom_keycodes {
  */
 
  [_LAYER0] = LAYOUT(
-         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_BSPC,
-         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_ENT,
+         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_EQL,
+         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_MINS,
          KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    LT(1,KC_SCLN), KC_QUOT,
   LSFT_T(KC_ESC),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   MO(3),   TG(2),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT),
                            KC_ESC,    KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC, KC_MINS, KC_UNDS
