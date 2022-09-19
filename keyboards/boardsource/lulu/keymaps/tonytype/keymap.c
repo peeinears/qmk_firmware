1e9169642998d7ac6ddeac893b06b237d8c3a398
Ian Pearce <ian@ianpearce.org>
2022-09-19 09:25:47 -0700

    Fix shifts

--- keyboards/boardsource/lulu/keymaps/tonytype/keymap.c
+++ keyboards/boardsource/lulu/keymaps/tonytype/keymap.c
@@ -31,10 +31,10 @@ enum custom_keycodes {
  */
 
  [_LAYER0] = LAYOUT(
-            KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_BSPC,
-            KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_ENT,
-            KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    LT(1,KC_SCLN), KC_QUOT,
-MT(KC_LSFT, KC_ESC),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   MO(3),   TG(2),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(KC_RSFT,KC_ENT),
+         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_BSPC,
+         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_ENT,
+         KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    LT(1,KC_SCLN), KC_QUOT,
+  LSFT_T(KC_ESC),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   MO(3),   TG(2),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT),
                            KC_ESC,    KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC, KC_MINS, KC_UNDS
 ),
 
